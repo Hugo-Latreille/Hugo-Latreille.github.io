5c2f0695754fc7b2265760199de5491f04e17fd6
Hugo Latreille <hugo.latreille@gmail.com>
2022-03-26 11:40:24 +0100

    création affichage  claver v1 en JS

--- sutom/index.js
+++ sutom/index.js
@@ -28,21 +28,38 @@ const createBox = () => {
 	}
 };
 
-const createKeyboard = () => {
-	let topKb = "AZERTYUIOP";
-	let middleKb = "QSDFGHJKLM";
-	let lowKb = "WXCVBN";
-	let keyboard = document.createElement("div");
-	keyboard.classList.add("keyboardContainer");
+const keyboard = document.querySelector(".keyboard");
+let topKb = "AZERTYUIOP";
+let middleKb = "QSDFGHJKLM";
+let lowKb = "WXCVBN";
+
+const createKeyboard = (layout, className) => {
+	let keyRow = document.createElement("div");
+	keyRow.classList.add("keyRows");
+	keyRow.classList.add(className);
+	keyboard.appendChild(keyRow);
+
+	createKeyboardKeys(className, layout);
+};
+
+const createKeyboardKeys = (className, layer) => {
+	const parent = document.querySelector(`.${className}`);
+	for (let i = 0; i < layer.length; i++) {
+		let key = document.createElement("div");
+		key.classList.add("key");
+		key.classList.add(`${layer[i]}`);
+		key.textContent = layer[i];
+		parent.appendChild(key);
+	}
 };
 
 drawBoard();
-//* créer des rangées div
-//* chaque rangée, boucle pour div chaque touche
-//* mise en forme CSS
-//* chaque touche, event mouse
+createKeyboard(topKb, "topKbDiv");
+createKeyboard(middleKb, "middleKbDiv");
+createKeyboard(lowKb, "lowKbDiv");
 
-//* fonction event clavier => chaque frappe écrit dans une box puis suivante une seule ligne. Valider envoie la donnée.
+//* chaque touche, event mouse click ==> hover + afficher lettre dans le board
+//* kb event général ==> afficher lettre dans le board, sur une ligne
 
 //? puis s'occuper logique choix aléatoire du mot et vérification
 //? mise en place du système de couleurs
